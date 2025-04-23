35edf8e79c11d1d9a6459948d4ec58754f3b321f
CuongNM3110 <cuong.nm@zopost.vn>
2025-04-23 18:40:45 +0700

    commit

--- tts-service/tts.py
+++ tts-service/tts.py
@@ -23,11 +23,9 @@ def tts_infer():
         "--model", "F5TTS_Base",
         "--gen_text", text,
         "--speed", str(speed),
-        "--vocoder_name", "vocos",
-        "--load_vocoder_from_local",  # ✅
+        "--vocoder_name", "bigvgan",
         "--vocab_file", os.path.join(model_dir, "vocab.txt"),
         "--ckpt_file", os.path.join(model_dir, "model_500000.pt"),
-        "--vocoder_local_path", "/vocoder",  # ✅
         "-w", output_wav  # ✅ Thay vì --output_path
     ]
 
